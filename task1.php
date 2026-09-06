d9a240446d3a312dbdf6e02f59c10ce0892823c1
kurkumuzz <anelelizhanova@gmail.com>
2026-09-06 21:05:41 +0500

    Solved 4 tasks

---
+++ task1.php
@@ -0,0 +1,20 @@
+<?php
+function task1(int $n): int
+{
+    if ($n < 0) {
+        return $n * $n;
+    } elseif ($n > 0) {
+        return $n * $n * $n;
+    }
+
+    return $n;
+}
+
+$numbers = [0, 8, -8];
+$current = 1;
+
+foreach ($numbers as $n) 
+{
+    echo $current . " result: " . task1($n) . PHP_EOL;
+    $current++;
+}
